5a1be986aee8a825890df9de140d382a00dcdb06
soniasingh98 <88448499+soniasingh98@users.noreply.github.com>
2025-12-12 22:41:23 +0530

    used langchain-corea

--- backend/utils.py
+++ backend/utils.py
@@ -2,8 +2,8 @@ import os
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from typing import List
-from langchain.prompts import PromptTemplate
-from langchain.output_parsers import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field, field_validator
 from fastapi import HTTPException
 
